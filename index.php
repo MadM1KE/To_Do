8ea74d7a3152c55a55e933e7c4df9c9fc4f9f907
MadM1KE <mishka199307@gmail.com>
2014-01-08 23:13:44 +0400

    change

--- index.php
+++ index.php
@@ -14,6 +14,27 @@
               <div></div>
               <input name="submit" type="submit">
 </form>
+<script>
+    $(document).on('click' ,'button' ,function() {
+    event.preventDefault();
+	   
+       var response1 =  $.post("serverside.php", {delete : this.id});
+       response1.done(function (data){
+     $('#div').replaceWith(data);     
+         }); 
+ });
+
+    $("#form").submit(function( event ){
+    event.preventDefault();
+    var information = $("#form").serialize();
+       var response =  $.post("serverside.php", information); 
+     
+	  response.done(function (data){
+     $('#div').replaceWith(data);     
+         }); 
+		 });
+
+</script
 
 <div id="div"></div>
 
