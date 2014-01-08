673610136251745fe82ff3217bf1d89a0bb98737
MadM1KE <mishka199307@gmail.com>
2014-01-08 22:05:48 +0400

    1st

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script>
+   <body>
+       <form id="form" method ="POST" action="serverside.php" >
+              <p>Item:</p><br/>
+              <textarea name="item" value="yes"> </textarea>
+              <p>Date:</p><br/>
+              <input name="date" type="Date">
+              <div></div>
+              <input name="submit" type="submit">
+</form>
+
+<div id="div"></div>
+
+</body>
+
+</html>
